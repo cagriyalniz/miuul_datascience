125edd24c7707b99ab029da47300ad7ff26b2b14
cagriyalniz <cagriyalniz@gmail.com>
2023-03-20 14:54:45 +0300

    case_study2 finished

--- Case_Study_II.py
+++ Case_Study_II.py
@@ -131,19 +131,60 @@ titanic_df_witout_who = df.drop("who", axis=1)
 print(titanic_df_witout_who.head())
 
 #Görev 13: deck değikenindeki boş değerleri deck değişkenin en çok tekrar eden değeri (mode) ile doldurunuz.
-mode_val = df['deck'].mode()[0]
+mode_val = df['deck'].mode()[0]#mode()[0] modun ilk elemanını döndürürken, mode()[1] IndexError hatası verir. Çünkü mode() sonucu bir tuple döndürür ve modun kendisi tuple'ın ilk elemanındadır. Eğer mode() sonucunda birden fazla mod değeri varsa, bu durumda ikinci mod değeri tuple'ın ikinci elemanında olacaktır.
 df_not_null_deck = df['deck'].fillna(mode_val)
 print(df_not_null_deck.head())
 
 #Görev 14: age değikenindeki boş değerleri age değişkenin medyanı ile doldurunuz.
+med_val = df['age'].median()
+df_not_null_age = df['age'].fillna(med_val)
+print(df_not_null_age)
+
 #Görev 15: survived değişkeninin pclass ve cinsiyet değişkenleri kırılımınında sum, count, mean değerlerini bulunuz.
+#"Kırılım" belirli bir kategoriye ait verilerin alt kategorilere ayrılarak analiz edilmesi anlamına gelir. Örneğin, bir veri setindeki kişilerin yaş, cinsiyet ve gelir düzeyleri bilgileri varsa, yaş kategorisi altında kadın ve erkeklerin gelir düzeylerine ayrılarak analiz edilebilir. Bu durumda, yaş kategorisi cinsiyet kırılımında incelenmiş olur.
+pclass_sex_ = df.groupby(["pclass", "sex"])["survived"].agg(["sum", "count", "mean"])
+print(pclass_sex_)
+#agg() fonksiyonu, bir DataFrame üzerinde birleştirme işlevleri uygulamak için kullanılır. Özellikle, bu fonksiyon, bir veya daha fazla sütuna uygulanacak bir veya daha fazla işlev belirlemenizi sağlar ve sonuç olarak birleştirilmiş bir DataFrame döndürür. 
+
+
 #Görev 16: 30 yaşın altında olanlar 1, 30'a eşit ve üstünde olanlara 0 vericek bir fonksiyon yazın. Yazdığınız fonksiyonu kullanarak titanik veri
 #setinde age_flag adında bir değişken oluşturunuz oluşturunuz. (apply ve lambda yapılarını kullanınız)
-#Görev 17: Seaborn kütüphanesi içerisinden Tips veri setini tanımlayınız.
-#Görev 18: Time değişkeninin kategorilerine (Dinner, Lunch) göre total_bill değerlerinin toplamını, min, max ve ortalamasını bulunuz.
-#Görev 19: Günlere ve time göre total_bill değerlerinin toplamını, min, max ve ortalamasını bulunuz.
+def age_flag(x):
+    if x < 30:
+        return 1
+    else:
+        return 0
+df['age_flag'] = df['age'].apply(lambda x: age_flag(x))
+print(df.head())
 
+#Görev 17: Seaborn kütüphanesi içerisinden Tips veri setini tanımlayınız.
+tps = sns.load_dataset("tips")
+print(tps)
+
+#Görev 18: Time değişkeninin kategorilerine (Dinner, Lunch) göre total_bill değerlerinin toplamını, min, max ve ortalamasını bulunuz.
+import numpy as np
+grouped_tips = tps.groupby("time")["total_bill"].agg([np.sum, np.min, np.max, np.mean])
+print(grouped_tips)
+
+#Görev 19: Günlere ve time göre total_bill değerlerinin toplamını, min, max ve ortalamasını bulunuz.
+agg_func = {"total_bill":["sum", "min", "max", "mean"]}
+grouped = tps.groupby(["day", "time"]).agg(agg_func)
+print(grouped)
 #Görev 20: Lunch zamanına ve kadın müşterilere ait total_bill ve tip değerlerinin day'e göre toplamını, min, max ve ortalamasını bulunuz.
+subset = tps[["day", "time", "sex", "total_bill", "tip"]]
+lunch_female = subset[(subset["time"] == "Lunch") & (subset["sex"] == "Female")]
+lunch_female_grouped = lunch_female.groupby("day").agg({"total_bill": ["sum", "min", "max", "mean"], "tip": ["sum", "min", "max", "mean"]})
+print(lunch_female_grouped)
+
 #Görev 21: size'i 3'ten küçük, total_bill'i 10'dan büyük olan siparişlerin ortalaması nedir? (loc kullanınız)
+result = tps.loc[(tps["size"]< 3) & (tps["total_bill"] > 10), ["total_bill"]].mean()
+print(result)
+
 #Görev 22: total_bill_tip_sum adında yeni bir değişken oluşturunuz. Her bir müşterinin ödediği totalbill ve tip in toplamını versin.
+tps["total_bill_tip_sum"] = tps["total_bill"] + tps["tip"]
+print(tps.head())
+
 #Görev 23: total_bill_tip_sum değişkenine göre büyükten küçüğe sıralayınız ve ilk 30 kişiyi yeni bir dataframe'e atayınız.
+sorted_df = tps.sort_values(by='total_bill_tip_sum', ascending=False)
+top_30 = sorted_df.head(30).reset_index(drop=True)
+print(top_30)
